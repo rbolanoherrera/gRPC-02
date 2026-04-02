04b16f682959477d6277ca34dd78b0e41dc2c0ff
Rafael Bolaños <rbolanoherrera@gmail.com>
2026-04-02 17:18:50 -0500

    Configuración para Extension del protocolo Http 1 para consumo desde clientes web

--- src/GrpcMantenimiento/Program.cs
+++ src/GrpcMantenimiento/Program.cs
@@ -4,7 +4,8 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddGrpc();
+builder.Services.AddGrpc()
+.AddJsonTranscoding();
 
 builder.Services.AddDbContext<GrpcMantenimiento.Data.GrpcDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
